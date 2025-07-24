c0173462bb645d0469dc4975d3c14bbc08b76520
sangdm <sangdm2507@gmail.com>
2025-07-24 19:02:17 +0700

    fix

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Đây là header</title>
+    <title>DỰ ÁN MẪU - Đỗ Minh Sáng</title>
 </head>
 <body>
-    <h1>Đỗ Minh Sáng</h1>
+    <h1>Đây là header</h1>
     <nav>
         <a href="index.php">Trang chủ</a> |
         <a href="index.php?action=products">Sản phẩm</a>
